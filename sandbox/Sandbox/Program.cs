e2167fdb8ef1f5970ca8d5e41e99480277ba0479
C Gibbons <gibbonsc@byui.edu>
2026-04-27 16:38:20 -0600

    Another sandbox: Sandbox2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // I can comment about this! My opnion matters!
         Console.WriteLine("Hello Sandbox World!");
+        for (int i = 1; i < 10; i += 2)
+        {
+            Console.WriteLine($"something {i}");
+        }
     }
 }
---
+++ sandbox/Sandbox2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Sandbox2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Goodbye, Mars!");
+    }
+}
